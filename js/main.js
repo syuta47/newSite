c579da89f8004415bdad591eb3316a271378afdb
syuta47 <53383746+syuta47@users.noreply.github.com>
2019-11-13 06:43:23 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,23 +6,30 @@ const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
 const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
 const sh = document.getElementById('sh');
 const sp = document.getElementById('sp');
 const sm = document.getElementById('sm');
 const sktImg = document.querySelector('.skt img');
 const desH1 = document.querySelector('.description h1');
 const c = document.querySelector('.count');
+const hokkaidouBtn = document.getElementById('h-btn');
+const hokkaidouImg = document.querySelector('img[src="img/541.png"]');
+const lastBtn = document.getElementById('message');
+const last = document.getElementById('last');
+const LB = document.getElementById('last-btn');
+
 
 btn1.addEventListener('click', () => {
-  sh.classList.add('active');
+  sh.classList.toggle('active');
 });
 
 btn2.addEventListener('click', () => {
-  sp.classList.add('active');
+  sp.classList.toggle('active');
 });
 
 btn3.addEventListener('click', () => {
-  sm.classList.add('active');
+  sm.classList.toggle('active');
 });
 
 btn4.addEventListener('click', () => {
@@ -31,21 +38,33 @@ btn4.addEventListener('click', () => {
 
 function remove() {
   desH1.classList.remove('active2');
+  a = true;
 }
 
-let i = 3;
+let i = 0;
+let a = true;
 
-function active() {
-  if(i === 0) {
+
+btn5.addEventListener('click', () => {
+  if (a !== true) {
     return;
   }
   desH1.classList.add('active2');
-  setTimeout(remove,3000);
-  const timeoutId = setTimeout(active,5000);
-  i--;
+  setTimeout(remove,2000);
+  i++;
   c.textContent = `Count ${i}`;
-}
+  a = false;
+});
 
-setTimeout(active,500);
+hokkaidouBtn.addEventListener('click', () => {
+  hokkaidouImg.classList.toggle('hokkaidou');
+});
+
+lastBtn.addEventListener('click', () => {
+  last.classList.add('active');
+});
+LB.addEventListener('click', () => {
+  last.classList.remove('active');
+});
 
 }
